33952558d643bb604dccdb963eb9f5e5d69b3b84
Gabie-21 <ngalasagabriella@gmail.com>
2024-03-14 21:25:11 +0300

    Added basic search engine behavior

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function handlesearchSubmit(event){
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city")
+    cityElement.innerHTML = searchInput.value;
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handlesearchSubmit)
