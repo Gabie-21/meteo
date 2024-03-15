a1c605b0a029b8c19e9435594bff90ed6a558e27
Gabie-21 <ngalasagabriella@gmail.com>
2024-03-15 13:13:58 +0300

    added advanced Javascript functionality to the project

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ function refreshweather(response){
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
+
+    let iconElement = document.querySelector("#icon");
+
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url} " class="weather-app-icon">`
     
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
