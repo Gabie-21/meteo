bcb3d068dcb04aedbcf6c978ca4305db13cd9587
Gabie-21 <ngalasagabriella@gmail.com>
2024-03-15 12:48:39 +0300

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,10 +2,40 @@ function refreshweather(response){
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     cityElement = document.querySelector("#city");
-
+    let descriptionElement = document.querySelector("#description");
+    let humididtyElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    
     cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humididtyElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatDate(date);
     temperatureElement.innerHTML = Math.round(temperature);
 }
+function formatDate(date){
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday",
+         "Monday", 
+         "Tuesday", 
+         "Wednesday", 
+         "Thursday", 
+         "Friday", 
+         "Saturday"];
+         let day = days[date.getDay()];
+
+         if(minutes < 10 ) {
+            minutes = `0${minutes}`;
+         }
+
+         return `${day} ${hours}: ${minutes}`;
+
+}
+
 function searchCity(city){
     let apiKey = "dbo2dde1863fta62243f1bb03c43d733";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
